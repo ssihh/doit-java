6d88c4295e0579a1149f43660f4784cb44d3314b
ssihh <49466178+ssihh@users.noreply.github.com>
2024-01-10 22:05:29 +0900

    P11720_숫자의합

--- 001_숫자의합 구하기.java
+++ 001_숫자의합 구하기.java
@@ -1,24 +1,18 @@
-import java.util.Scanner;
-// tip: each public class is put in its own file
-public class main
-{
-    // tip: arguments are passed via the field below this editor
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //N 선언해서 뭐함
-        
-        //입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환하기
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
-        int sum = 0;
+package example;
 
-        for(int i = 0; i < cNum.length; i++){
-            sum+=cNum[i]-'0';
-        }
-        System.out.print(sum);
+import java.util.*;
 
-
-        //System.out.println("Hello, world!");
-    }
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // nextInt() Int타입으로 리턴
+		
+		String sNum = sc.next(); // next() String타입으로 리턴
+		char[] cNum = sNum.toCharArray(); //String -> char[] 변환
+		int sum=0;
+		for(int i =0; i<cNum.length; i++) {
+			sum +=cNum[i]-'0';
+		}
+		System.out.print(sum);
+	}
 }
