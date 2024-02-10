3245bdd99a5c4469524b147864d3efeafbe7d59d
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-10 16:01:55 +0900

    [Gold V] Title: 집합의 표현, Time: 2372 ms, Memory: 244384 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	private static int[] parent;
+	public static void main(String[] args) throws IOException {
+
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		parent = new int[n+1];
+		for(int i =0; i<=n;i++) {
+			parent[i] =i;
+		}
+		
+		for( int j=0; j<m;j++ ) {
+			int q = sc.nextInt();
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(q==0) { // union
+				union(a,b);
+			}
+			else { // a,b 대표노드가 같은지
+				checkSame(a,b);
+			}
+		}
+		
+
+	}
+	private static void union(int a, int b) {
+		//대표노드를 찾아서 연결
+		int ua = find(a);
+		int ub = find(b);
+		int min = Math.min(ua,  ub);
+		if(ua!=ub) {
+			parent[ub] = ua; 
+		}
+		
+	}
+	
+	private static int find(int i) {
+		if(i == parent[i]) {
+			return i;
+		}
+		else {
+			return parent[i] = find(parent[i]);
+			//경로압축 로직: 빠져나올때마다 그때의 대표노드 값을 업데이트
+		}
+		
+	}
+
+	private static void checkSame(int a, int b) {
+		if(find(a)==find(b)) {
+			System.out.println("YES");
+		}
+		else {
+			System.out.println("NO");
+		}
+	}
+
+}
