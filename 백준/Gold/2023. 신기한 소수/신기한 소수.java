4abfec897c36803a68686e4874778670d803cd12
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-12 11:00:07 +0900

    [Gold V] Title: 신기한 소수, Time: 496 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,60 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
+	static ArrayList<Integer>[] a;
+	static boolean[] visited;
 	static int n;
+
 	public static void main(String[] args) throws IOException {
-		// 들어오는 데이터가 많을 때는 Scanner보다 br이 시간복잡도에서 유리
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		// 출력을 버퍼에 넣고 한번에 출력하기 위해 BufferedWriter사용
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		
-//		int n = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
 //		int m = Integer.parseInt(st.nextToken());
-//		int n = Integer.parseInt(br.readLine()); //br만 선언해서 한줄씩
 
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		
-		DFS(2,1); //일의자리 소수는 2,3,5,7이므로 4개 수에서만 시작
-		DFS(3,1);
-		DFS(5,1);
-		DFS(7,1);
-		
+		// 인접리스트
+		a = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = new ArrayList<Integer>();
+		}
+
+		// 방문 배열
+		visited = new boolean[n + 1];
+
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
+
 	}
-		
 
-	private static void DFS(int num, int jarisu) {
-		// TODO Auto-generated method stub
-		if(jarisu ==n) { //목표자리수일때
-			if(isPrime(num)) {
-				System.out.println(num); //소수면 숫자 출력
+	private static void DFS(int num, int jari) {
+		if (jari == n) {
+			System.out.println(num);
+		}
+
+		for (int i = 1; i <= 9; i++) {
+			if (i % 2 != 0 && isPrime(num * 10 + i)) {
+				DFS(num * 10 + i, jari + 1);
 			}
-			return; //재귀함수 빠져나가
 		}
-		
-		for(int i=1; i<10; i++) {
-			if(i%2==0) {
-				continue; //짝수라면 더 이상 탐색할 필요가 없음
-			} //1,3,5,7,9
-			
-			if(isPrime(num*10+i)) { 
-				DFS(num*10+i, jarisu+1);
-			} //21,23,25... / 2
-		}
+
 	}
 
-
-	private static boolean isPrime(int num) { //소수 판별 암기
-		// TODO Auto-generated method stub
-		for(int i=2; i<num/2;i++) { // 2~num/2 까지 나누어지는 수가있으면 F
-			if(num%i==0) {
+	private static boolean isPrime(int num) {
+		for (int i = 2; i <= num / 2; i++) {
+			if (num % i == 0) {
 				return false;
 			}
 		}
-		return true; //소수면 true
+		return true;
+
 	}
 
 }
