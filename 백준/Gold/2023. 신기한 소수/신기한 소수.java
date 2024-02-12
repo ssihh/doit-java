39436e3c922cdc90bc69820f7ed7bd7305e452db
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-12 11:03:19 +0900

    [Gold V] Title: 신기한 소수, Time: 484 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -22,6 +22,7 @@ public class Main {
 		// 방문 배열
 		visited = new boolean[n + 1];
 
+		// (소수, 1번째 자리수부터 체크)
 		DFS(2, 1);
 		DFS(3, 1);
 		DFS(5, 1);
@@ -30,13 +31,14 @@ public class Main {
 	}
 
 	private static void DFS(int num, int jari) {
-		if (jari == n) {
+		if (jari == n) { //4자리수 일때 출력
 			System.out.println(num);
+			return;
 		}
 
 		for (int i = 1; i <= 9; i++) {
-			if (i % 2 != 0 && isPrime(num * 10 + i)) {
-				DFS(num * 10 + i, jari + 1);
+			if (i % 2 != 0 && isPrime(num * 10 + i)) { 
+				DFS(num * 10 + i, jari + 1); //소수일때 자리수++해서 재귀 실행
 			}
 		}
 
