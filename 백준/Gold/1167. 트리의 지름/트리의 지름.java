f705ba9029eb865a6f0a0b21765ab4f9740d5226
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-12 22:17:33 +0900

    [Gold II] Title: 트리의 지름, Time: 1008 ms, Memory: 107016 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,101 +1,92 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Edge{ //class는 Main밖에다가 입력
-	int e; //목적지 노드 O
-	int value; //에지의 가중치 ㅡ 간선길이
-	public Edge(int e, int value) { //i.o; | i.value;
-		this.e=e;
-		this.value = value;
+class Edge {
+	int v;
+	int w;
+
+	public Edge(int v, int w) { // public Edge | new Edge(v,w)
+		this.v = v;
+		this.w = w;
 	}
 }
 
 public class Main {
 
-	static boolean[] visited; // 방문도 차원
-	static int[] distance; //거리 저장 배열
 	static ArrayList<Edge>[] a;
-	static int n, m;
-
-
-	
-//	static boolean arrive;
-//	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static boolean[] visited;
+	static int[] distance;
+	static int n;
 
 	public static void main(String[] args) throws IOException {
-		// 들어오는 데이터가 많을 때는 Scanner보다 br이 시간복잡도에서 유리
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 출력을 버퍼에 넣고 한번에 출력하기 위해 BufferedWriter사용
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		
-//		n = Integer.parseInt(st.nextToken());
-//		m = Integer.parseInt(st.nextToken());
-//		int v = Integer.parseInt(st.nextToken());
-//		int n = Integer.parseInt(br.readLine()); //br만 선언해서 한줄씩
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
 
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		
-
-		
-		a = new ArrayList[n+1]; //노드 개수만큼
-		for(int i =1; i<n+1;i++) {
-			a[i]=new ArrayList<Edge>();
+		a = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = new ArrayList<Edge>(); // new 뉴뉴
 		}
-		
-		for(int j=0;j<n;j++) { //A인접리스트에 그래프 데이터 저장하기
-			int s = sc.nextInt(); //start
-			while(true) {
-				int e=sc.nextInt(); //end
-				if(e==-1) {
+
+		// 입력 마지막 -1일때 다음줄 읽어
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			while (true) {
+				int e = Integer.parseInt(st.nextToken());
+				if (e == -1) {
 					break;
 				}
-				int value = sc.nextInt();
-				a[s].add(new Edge(e, value));
+				int w = Integer.parseInt(st.nextToken());
+				a[s].add(new Edge(e, w)); // Edge 값 add하는 법
+				// a[e].add(new Edge(i, w));
+
 			}
 		}
-		
-		visited = new boolean[n+1];
-		distance = new int[n+1];
+
+		// 방문 배열, 거리 배열
+		visited = new boolean[n + 1];
+		distance = new int[n + 1];
+
+		int maxboyu = 1; // 트리의 지름 max 보유 배열[answer] 번째 값
+
+		// BFS실행
 		BFS(1);
-		int max=1;
-		for(int i=2;i<=n;i++) {
-			if(distance[max] < distance[i]) {
-				max=i; //트리의지름에 해당되는 2개의 노드중 하나로 시작점 설정
+
+		for (int i = 2; i <= n; i++) {
+			if (distance[maxboyu] < distance[i]) {
+				maxboyu = i;
 			}
 		}
-		
-		visited = new boolean[n+1]; //6
-		distance = new int[n+1];
-		BFS(max);
-		Arrays.sort(distance); //Array's'.어레이스
-		System.out.println(distance[n]); //0,1,2,3,4,5
-		
-		
+
+		// 방문 배열, 거리 배열 다시 초기화 하고 BFS
+		visited = new boolean[n + 1];
+		distance = new int[n + 1];
+		BFS(maxboyu);
+
+		Arrays.sort(distance); // ㄷ ㄷ정렬해서 큰값 뒤로 가게하고
+		System.out.println(distance[n]); // 그 큰값을 출력해버림
+
 	}
 
-	private static void BFS(int node) {
-		Queue<Integer> q = new LinkedList<>(); // 데이터가 두개들어가니까 배열로
-		q.offer(node); // 노드 처음시작점을 넣어주는것(0,0)
-		visited[node] = true; //방문 t
-		
+	private static void BFS(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		visited[v] = true;
+
 		while (!q.isEmpty()) {
-			int now = q.poll();
-			
-			for(Edge i : a[now]) {
-				int ne = i.e;  //괄호 필요없네
-				int nv = i.value;
-				if(!visited[ne]) {
-					q.offer(ne);
-					visited[ne]=true;
-					distance[ne]= distance[now] + nv;
+			int crt = q.poll();
+			for (Edge i : a[crt]) {
+				int iv = i.v;
+				int iw = i.w;
+				if (!visited[iv]) {
+					q.offer(iv);
+					visited[iv] = true;
+					distance[iv] = distance[crt] + iw;
 				}
-				
 			}
-
 		}
-	}
 
+	}
 
 }
