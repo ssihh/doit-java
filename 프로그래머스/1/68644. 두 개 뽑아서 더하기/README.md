9d046d6013c3b0369e4b3401023952398ea658d1
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-15 19:06:05 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.13 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 1.12 ms
+메모리: 75.2 MB, 시간: 1.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 16:9:39
+2024년 2월 4일 19:6:3
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,27 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-       
-        HashSet<Integer> hashset = new HashSet<>();
-        for(int i =0; i<numbers.length-1; i++)
-            for(int j=i+1; j<numbers.length; j++){
-                hashset.add(numbers[i]+numbers[j]);
+        int[] answer;
+        
+        int n = numbers.length;
+        HashSet<Integer> hs = new HashSet<>(); //Int
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                hs.add(numbers[i]+numbers[j]); //hs.add()
             }
-        
-        
-        int[] answer = new int[hashset.size()];
-        Iterator<Integer> iterator = hashset.iterator();
-
-        // for (int j=0; j< hashset.size(); j++){
-        //     answer[j] = (int)iterator.next();
-        // }
-        
-        int k=0;
-        while(iterator.hasNext()){
-            answer[k++]=iterator.next();
         }
         
-        Arrays.sort(answer);
+        Iterator<Integer> iter = hs.iterator(); //<Integer> 해도되고 안해도되고
+        answer = new int[hs.size()]; //hs.size(); answer 크기 초기화 해줘야 오류 안남
+        
+        int c=0;
+        while(iter.hasNext()){ //iter.hasNext()
+            answer[c++]=iter.next();  //iter.next()
+        }
+
+        Arrays.sort(answer); //정렬
         return answer;
     }
 }
