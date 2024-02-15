0cf12d21dc675511f31a593dcefbbe009a743cd7
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-15 21:25:55 +0900

    [level 2] Title: 모음 사전, Time: 10.35 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,31 @@
+import java.util.*; //에러뜨면 import 해보자
+
+class Solution {
+    String[] alpha = {"A", "E", "I", "O", "U"};
+    ArrayList<String> words; 
+    //List 인터페이스를 구현한 클래스이기 때문에 저장된 값의 순서를 유지하고 중복을 허용한다. 
+    
+    public void create_words(int lev, String str) { //0,""
+        words.add(str); //words.get(i) = "" : 첫번째(0) 값은 ""임
+        for (int i = 0; i < 5; i++) {
+            if (lev < 5) { //lev 0
+                create_words(lev+1, str.concat(alpha[i])); //A
+            }
+            else return;
+        }
+    }
+    
+    public int solution(String word) {
+        int answer = 0;
+        words  = new ArrayList<String>();
+        create_words(0, ""); //함수 호출
+        for (int i = 0; i < words.size(); i++) {
+            if (word.equals(words.get(i))) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+
