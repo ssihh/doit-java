6ebdec484b27b9e371de779b5749e4a221d4e9c4
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-16 17:27:15 +0900

    [Gold V] Title: 토마토, Time: 700 ms, Memory: 120624 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-
+//visited 없는 ver
 	public static int n;
 	public static int m;
-	public static boolean[][] visited;
 	public static int[][] map;
 	public static int t0;
 	public static Queue<int[]> q = new LinkedList<>();
@@ -26,9 +25,8 @@ class Main {
 				int nx = cx + dx[i];
 				int ny = cy + dy[i];
 				if(nx>=0 && ny>=0 && nx<m && ny<n) { //좌표 유효성 검사
-					if(map[nx][ny]==0 && !visited[nx][ny]) {
+					if(map[nx][ny]==0) {
 						q.offer(new int[] {nx, ny});
-						visited[nx][ny] = true;
 						map[nx][ny] = map[cx][cy] + 1;
 						t0 --;
 						day = map[nx][ny]; //출력값
@@ -48,7 +46,6 @@ class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
-		visited = new boolean[m][n];
 		map = new int[m][n];
 
 		for (int i = 0; i < m; i++) { //행
@@ -62,7 +59,7 @@ class Main {
 				else if(t == 1) { //출발지가 여러갠데 어카지
 					q.offer(new int[] {i,j}); // 이렇게 바로 
 //					System.out.println(i+", "+j);
-					visited[i][j] = true;
+
 				}
 			}
 		}
