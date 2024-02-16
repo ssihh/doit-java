6b6fb2b1da1a56da37c4919e03243ec9ebdff420
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-16 23:43:30 +0900

    [level 2] Title: 모음 사전, Time: 1.83 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,40 @@
-import java.util.*; //에러뜨면 import 해보자
+import java.util.*;
+import java.io.*;
 
 class Solution {
-    String[] alpha = {"A", "E", "I", "O", "U"};
-    ArrayList<String> words; 
-    //List 인터페이스를 구현한 클래스이기 때문에 저장된 값의 순서를 유지하고 중복을 허용한다. 
+    public String[] alpha = {"A", "E", "I", "O", "U"};
+    public ArrayList<String> sazon = new ArrayList<>();
     
-    public void create_words(int lev, String str) { //0,""
-        words.add(str); //words.get(i) = "" : 첫번째(0) 값은 ""임
-        for (int i = 0; i < 5; i++) {
-            if (lev < 5) { //lev 0
-                create_words(lev+1, str.concat(alpha[i])); //A
-            }
-            else break;
+    public void DFS(int len, String str){
+        
+        sazon.add(str); //저장하고
+        
+        if(len == 5) { // 5면 아래 for문 수행안하고 리턴 메소드 종료
+            return;
         }
+        
+        for(int i=0; i<5; i++){
+            if(len<5){
+                DFS(len+1, str.concat(alpha[i]));
+            }
+        }
+        
     }
     
     public int solution(String word) {
         int answer = 0;
-        words  = new ArrayList<String>();
-        create_words(0, ""); //함수 호출
-        for (int i = 0; i < words.size(); i++) {
-            if (word.equals(words.get(i))) {
+        
+        DFS(0, "");
+        
+        int n = sazon.size();
+        
+        for(int i=0; i<n; i++){
+            if(word.equals(sazon.get(i))){
                 answer = i;
                 break;
             }
         }
+        
         return answer;
     }
 }
-
